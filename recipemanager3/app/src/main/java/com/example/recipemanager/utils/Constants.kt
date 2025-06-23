0694f0de9a32518e691dc7946a3aa792d6cc3a08
Lukatc <lukachalidze7878@gmail.com>
2025-06-23 20:13:42 +0400

    Delete recipemanager3 directory

--- recipemanager3/app/src/main/java/com/example/recipemanager/utils/Constants.kt
+++
@@ -1,7 +0,0 @@
-package com.example.recipemanager.utils
-
-object Constants {
-    const val DEFAULT_SERVINGS = 1
-    const val DEFAULT_DIFFICULTY = "Easy"
-    const val DEFAULT_CATEGORY = "Main Course"
-}
--- recipemanager3/build.gradle.kts
+++
@@ -1,7 +0,0 @@
-plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.kotlinAndroid) apply false
-    alias(libs.plugins.kotlinKapt) apply false
-    alias(libs.plugins.googleGmsGoogleServices) apply false
-    alias(libs.plugins.androidxNavigationSafeargsKotlin) apply false
-}
